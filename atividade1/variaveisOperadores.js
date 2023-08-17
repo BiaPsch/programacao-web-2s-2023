ac45fce1c547d234fbeaa7adeeccb0f0b8290c16
ANA BEATRIZ ALVES PASCHOAL <ana.bpaschoal@a.ucb.br>
2023-08-17 10:35:13 -0300

    Adiciona atividade 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,19 @@
+// Declaração das variáveis numéricas
+var num_A = 10;
+var num_B = 5;
+
+// Operador de adição (+)
+var num_C_adicao = num_A + num_B;
+console.log(`Resultado da adição entre ${num_A} e ${num_B} = ${num_C_adicao}`);
+
+// Operador de subtração (-)
+var num_C_subtracao = num_A - num_B;
+console.log(`Resultado da subtração entre ${num_A} e ${num_B} = ${num_C_subtracao}`);
+
+// Operador de multiplicação (*)
+var num_C_multiplicacao = num_A * num_B;
+console.log(`Resultado da multiplicação entre ${num_A} e ${num_B} = ${num_C_multiplicacao}`);
+
+// Operador de divisão (/)
+var num_C_divisao = num_A / num_B;
+console.log(`Resultado da divisão entre ${num_A} e ${num_B} = ${num_C_divisao}`);
